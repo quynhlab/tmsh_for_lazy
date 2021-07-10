cc7ec4229fd1455408814a58f9b96db8fbf634db
quynhlab <80056481+quynhlab@users.noreply.github.com>
2021-07-10 14:51:32 +0700

    Update lazyboy.sh

--- lazyboy.sh
+++ lazyboy.sh
@@ -4,6 +4,7 @@ port='port.txt'
 
 for portservice in `cat $port`
 do
-	echo "tmsh create ltm pool Pool_DCST_$portservice members add { 10.163.135.74:$portservice 10.163.135.75:$portservice } monitor tcp"
-	echo "tmsh create ltm virtual vs_14.225.247.11_$portservice destination 14.225.247.11:$portservice profiles add { f5-tcp-wan { context clientside } f5-tcp-lan { context serverside } } source-address-translation { type snat pool SNAT_VLAN19 } pool Pool_DCST_$portservice"
+	tmsh create ltm pool Pool_DCST_$portservice members add { 10.163.135.74:$portservice 10.163.135.75:$portservice } monitor tcp
+	tmsh create ltm virtual vs_14.225.247.11_$portservice destination 14.225.247.11:$portservice profiles add { f5-tcp-wan { context clientside } f5-tcp-lan { context serverside } } source-address-translation { type snat pool SNAT_VLAN19 } pool Pool_DCST_$portservice
+	echo "Create portservice $portservice done!"
 done;
